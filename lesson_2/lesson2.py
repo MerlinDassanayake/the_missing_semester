aaa4e2fe7b334528bc9ae196d69f58b103a7aea7
MerlinDassanayake <dinushadassa@gmail.com>
2025-08-05 17:02:37 -0400

    update

--- lesson_2/lesson2.py
+++ lesson_2/lesson2.py
@@ -15,3 +15,41 @@ sc.pp.log1p(adata)
 sc.pp.highly_variable_genes(adata, flavor='seurat',
                             n_top_genes=2000)
 adata = adata[:, adata.var.highly_variable]
+
+# reduce dims
+sc.pp.scale(adata)
+sc.tl.pca(adata, svd_solver='arpack')
+sc.pp.neighbors(adata, n_neighbors=10)
+sc.tl.umap(adata)
+sc.tl.leiden(adata)
+
+sc.pl.spatial(adata, color='leiden', spot_size=100)
+
+# Identify marker genes
+sc.tl.rank_genes_groups(adata, 'leiden', method='t-test')
+sc.pl.rank_genes_groups(adata, n_genes=5, sharey=False)
+
+# spatial neighborhood enrichment
+sq.gr.spatial_neighbors(adata)
+sq.gr.spatial_autocorr(adata, mode='moran')
+
+# convert result to df
+moran_df = pd.DataFrame(adata.uns['moranI'])
+
+# set gene names as col (instead of index)
+moran_df = moran_df.reset_index().rename(columns={"index":"gene"})
+
+# sort by Moran's I value
+moran_df_sorted = moran_df.sort_values("I", ascending=False)
+
+# plot top 20 genes
+top_n = 20
+plt.figure(figsize=(10,6))
+plt.barh(
+    moran_df_sorted['gene'][:top_n][::-1], # reverse for descending y-axis
+    moran_df_sorted['I'][:top_n][::-1]
+)
+plt.xlabel("Moran's I")
+plt.title(f"Top {top_n} Genes by Spatial Autocorrelation")
+plt.tight_layout()
+plt.show()
